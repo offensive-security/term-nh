dce4c572cf7b95c5fd881b9068a0fc523e16a805
fuenor <fuenor@gmail.com>
2016-08-01 02:05:49 +0900

    Fix font width

--- emulatorview/src/main/java/com/offsec/nhterm/emulatorview/PaintRenderer.java
+++ emulatorview/src/main/java/com/offsec/nhterm/emulatorview/PaintRenderer.java
@@ -143,5 +143,5 @@ class PaintRenderer extends BaseTextRenderer {
     private int mCharHeight;
     private int mCharAscent;
     private int mCharDescent;
-    private static final char[] EXAMPLE_CHAR = {'X'};
+    private static final char[] EXAMPLE_CHAR = {'M'};
 }
