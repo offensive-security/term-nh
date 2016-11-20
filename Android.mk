a25ff7f0d4b654870ffb4d902388af0aec591036
binkybear <binkybear@nethunter.com>
2016-11-20 10:25:29 -0600

    Rename module to Terminal
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- Android.mk
+++ Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := Nethunter-Terminal-Emulator
+LOCAL_MODULE := Terminal
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := Nethunter-Terminal-Emulator
+LOCAL_PACKAGE_NAME := Terminal
 
 nhterm_root  := $(LOCAL_PATH)
 nhterm_dir   := term
