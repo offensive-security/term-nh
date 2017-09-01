fb2ee1f288daf3738d4a8144a4263c51d848501e
binkybear <binkybear@nethunter.com>
2017-09-01 08:31:38 -0500

    Build libs, fixed so library name
    Tested and terminal works now even without Kali
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- term/src/main/jni/Android.mk
+++ term/src/main/jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 # This is the target being built.
-LOCAL_MODULE:= libjackpal-androidterm5
+#LOCAL_MODULE:= libjackpal-androidterm5
+LOCAL_MODULE:= libjackpal-androidterm5nhj1
 
 # All of the source files that we will compile.
 LOCAL_SRC_FILES:= \
